bf2d048d07f3f9796bd7bc3fbceed5a95725cd33
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2025-10-05 06:55:52 -0300

    Update Untitled.py

---
+++ Untitled.py
@@ -0,0 +1,10 @@
+print("Hello word!")
+nome = input("Digite seu nome:")
+
+print(nome)
+
+#formatadores de caracteres
+print("Olá %s, parabens pelo seu primeiro hello word!" % (nome))
+
+#f-string
+print(f"Olá {nome}, parabens pelo seu primeiro hello word!")
